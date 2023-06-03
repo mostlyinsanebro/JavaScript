a3729042c557c550e6f03a168eed710a7caac448
Abhinav <67469292+mostlyinsanebro@users.noreply.github.com>
2023-06-03 18:04:48 +0530

    Add files via upload

---
+++ JavaScript Fundamentals - Part 1/Linking a JavaScript File/script.js
@@ -0,0 +1,8 @@
+let js = 'amazing'; // ; are optional in js
+
+if (js == 'amazing') alert('JS IS FUN!');
+
+// console.log() function. This function acts as a bridge between script and our browser console.
+
+// we can perform mathematical expressions directly in console.log() function.
+console.log(48+10+23-16);
