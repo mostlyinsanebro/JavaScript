966b67f75daf30dfdca52dcf8692547bf0f63a7f
Abhinav <67469292+mostlyinsanebro@users.noreply.github.com>
2023-06-04 00:10:48 +0530

    Create script.js

---
+++ JavaScript Fundamentals - Part 1 /10. Values and Variables /script.js
@@ -0,0 +1,33 @@
+let js = 'amazing'; 
+
+console.log(48+10+23-16);
+
+// values are the smallest piece of information in our code
+// and variables are basically used to store those vaues.
+// Vraibles are like boxes with tables and values are what is inside those boxes
+// and those values can be accessed useing the variable names.
+
+let firstName = 'Abhinav';
+console.log(firstName);
+console.log(firstName);
+
+// Also, for the syntax for variables, use camelcasing and also do not use
+// any special characters in the variables.
+
+let last$name = 'Prajapati';
+console.log(last$name);
+
+// let sir&name = 'Kumar'; -- will give error.
+// console.log(sir&name);
+// let 3name = 'Abhi';     -- will also give error.
+
+// & is not allowed in variable names and we can not styart them with numbers as well.
+// also, do not use reserved keywords. only _ and $ are allowed besides letters in naming variables.
+// Constants are written in uppercases.
+
+// variables should be sefl-explanatory
+
+let myFirstJob = 'Application Developer';
+let myCurrentJob = 'Front-end developer';
+
+console.log(myCurrentJob);
