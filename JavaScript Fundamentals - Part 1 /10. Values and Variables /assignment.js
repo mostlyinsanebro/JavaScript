e38a1b6e5e106c76d864d278123c0a95b1287cf8
Abhinav <67469292+mostlyinsanebro@users.noreply.github.com>
2023-06-04 00:30:57 +0530

    Create assignment.js

---
+++ JavaScript Fundamentals - Part 1 /10. Values and Variables /assignment.js
@@ -0,0 +1,7 @@
+let country = 'India';
+
+let continent = 'Asia';
+
+let population = '1400 million';
+
+console.log('Popoulation of '+country+' which comes in continent '+continent+' is '+population+'.');
