87bbc0295616a65dfb9e1ae57ebeb2cf7b7383be
Abhinav <67469292+mostlyinsanebro@users.noreply.github.com>
2023-06-20 00:14:40 +0530

    Create script.js

---
+++ JavaScript Fundamentals - Part 1 /18. Taking decisions : if - else Statements /script.js
@@ -0,0 +1,32 @@
+/*
+if else is used to control the execution of our code. We can make some of code execute and 
+ some of our code not execute using if else.
+ if else takes the conditions as inputs and these consitions return boolean as result
+ true or flaese, so if else takes boolean as input.
+ else is optional in if else.
+*/
+
+const age = 15;
+
+if(age >= 18)
+{
+   console.log("Sarah can have her driving license :)")
+}
+else
+{
+   console.log(`Sarah is too young. She will have to wait for another ${18 - age} years.`);
+}
+
+
+
+const year = 2023;
+let century;
+if(year >= 2000)
+{
+    century = 21;
+}
+else
+{
+    century = 21;
+}
+console.log(century);
