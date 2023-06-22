08fc01e68b915e518b71d9a24bb0f524c086c039
Abhinav <67469292+mostlyinsanebro@users.noreply.github.com>
2023-06-22 23:44:24 +0530

    Create script.js

---
+++ JavaScript Fundamentals - Part 1 /24. Logical Operators /script.js
@@ -0,0 +1,18 @@
+const hasDriversLicense = true;
+const hasGoodVision = true;
+const isTired = false;
+
+
+if(hasDriversLicense && hasGoodVision && !isTired) 
+console.log("Sarah is able to drive");
+
+else
+console.log("Someone else should drive...");
+
+
+// if else if works without an else.
+const a = 20;
+if(a === 5)
+console.log("a is 5");
+else if(a === 10)
+console.log("a is 10");
