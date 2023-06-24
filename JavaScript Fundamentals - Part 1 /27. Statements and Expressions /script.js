56032608b321655d3a43640727813c4def9d85cf
Abhinav <67469292+mostlyinsanebro@users.noreply.github.com>
2023-06-24 16:37:57 +0530

    Create script.js

---
+++ JavaScript Fundamentals - Part 1 /27. Statements and Expressions /script.js
@@ -0,0 +1,23 @@
+// Statements and Expressions
+
+// Expression is a piece of code that produces a value
+
+3+4 // expression
+1191 // expression 
+true && false && !false // expression
+
+// Statement is a bigger piece of code to be executed which does not 
+// produce a value on itself.
+
+// A devclaration is a sentence or statements and expressions are smaller piece that make up the sentence.
+// actions are statements.
+
+if(23>10)
+{
+    console.log('23 is greater');
+}
+
+// 23 > 10 is an expression 
+// whole if is a statement. // string '23 is greater' itself is also an expression.
+// something that ends with a ; is a statement.
+
