2ec317720ffa4c1f042c24f1482fd6ee517d2c2e
Abhinav <67469292+mostlyinsanebro@users.noreply.github.com>
2023-07-02 17:32:35 +0530

    Create script4.js

---
+++ Section 5: Developer Skills and Editor Setup/script4.js
@@ -0,0 +1,23 @@
+// 1. Understand the problem
+// We will be given an array and we have to create and return the desired string out of it.
+
+// 2. Divide (Smaller problems)
+// Write function
+// concatenate the string from the last iteration at the starting of current string.
+// return the string.
+
+// 3. Research
+// how to concatenate strings in js (+)
+
+// Solution:-
+const printForecast = function (arr) {
+  let result = '';
+
+  for (let i = 0; i < arr.length; i++) {
+    result = result + `... ${arr[i]}'C in ${i + 1} days `;
+  }
+  result = result + '...';
+  return result;
+};
+
+console.log(printForecast([12, 5, -5, 0, 4]));
