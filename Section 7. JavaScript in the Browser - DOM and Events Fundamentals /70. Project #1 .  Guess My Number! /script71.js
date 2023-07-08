f4b3c990906b3c1cbab7d8a1fa9f72ba08b3af44
Abhinav <67469292+mostlyinsanebro@users.noreply.github.com>
2023-07-08 22:48:49 +0530

    Create script71.js

---
+++ Section 7. JavaScript in the Browser - DOM and Events Fundamentals /70. Project #1 .  Guess My Number! /script71.js
@@ -0,0 +1,28 @@
+// DOM stands for Document Object Model. It is a structured representation of HTML documents
+// and it allows JS to access HTML elements and styles in order to manipulate them.
+
+// We will be able to change text, HTML attributes and even CSS styling with the help of DOM.
+
+// It is basically a connection point between js and the HTML documents and it enables us
+// to make changes to HTML document through js code.
+
+// DOM is automatically created by the browser as soon as an HTML page loads in it
+// and it is stored in a tree structure where each element has one node and nodes
+// has child nodes.
+
+// For each element in HTML document there is an element node in the DOM tree using which
+// we can interact with this element through js.
+
+// Document is an object in js which serves as an entry point to the DOM because we need to
+// select elements in HTML through js code methods like querySelector() are the methods of this object.
+
+// Whatever is there in an HTML Document, is also there in DOM.
+// So, DOM is the complete representation of an HTML document.
+
+// DOM !== JAVASCRIPT
+// The DOM and DOM methods are part of Web APIs and not js.
+
+// APIs are libraries that Browsers implement and have the DOM and DOM methods as a part of them
+// They can be accessed with our js code.
+
+// They are automatically available for us to use.
