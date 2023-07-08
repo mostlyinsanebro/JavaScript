21959f3b37faa82ad9fd63af05e34f8cf6524e80
Abhinav <67469292+mostlyinsanebro@users.noreply.github.com>
2023-07-08 22:46:18 +0530

    Delete Section 7: JavaScript in the Browser :  DOM and Events Fundamentals  directory

--- Section 7: JavaScript in the Browser :  DOM and Events Fundamentals /script70.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-// In order to select an element from the html page, we can use the selector
-// querySelector function to select the element using its .element or #id.
-// textContent is used to select the text content that is there in the tag.
-console.log(document.querySelector('.message').textContent);
