b44a7884a9cf9c9f6929a193e71866dae0b274c7
Abhinav <67469292+mostlyinsanebro@users.noreply.github.com>
2023-07-16 16:54:14 +0530

    Update script.js

--- JavaScript Fundamentals - Part 1 /10. Values and Variables /script.js
+++ JavaScript Fundamentals - Part 1 /10. Values and Variables /script.js
@@ -3,15 +3,15 @@ let js = 'amazing';
 console.log(48+10+23-16);
 
 // values are the smallest piece of information in our code
-// and variables are basically used to store those vaues.
-// Vraibles are like boxes with tables and values are what is inside those boxes
-// and those values can be accessed useing the variable names.
+// and variables are basically used to store those values.
+// Variables are like boxes with tables and values are what is inside those boxes
+// and those values can be accessed using the variable names.
 
 let firstName = 'Abhinav';
 console.log(firstName);
 console.log(firstName);
 
-// Also, for the syntax for variables, use camelcasing and also do not use
+// Also, for the syntax for variables, use camel casing and also do not use
 // any special characters in the variables.
 
 let last$name = 'Prajapati';
@@ -21,11 +21,11 @@ console.log(last$name);
 // console.log(sir&name);
 // let 3name = 'Abhi';     -- will also give error.
 
-// & is not allowed in variable names and we can not styart them with numbers as well.
+// & is not allowed in variable names and we can not start them with numbers as well.
 // also, do not use reserved keywords. only _ and $ are allowed besides letters in naming variables.
-// Constants are written in uppercases.
+// Constants are written in uppercase.
 
-// variables should be sefl-explanatory
+// variables should be self-explanatory
 
 let myFirstJob = 'Application Developer';
 let myCurrentJob = 'Front-end developer';
