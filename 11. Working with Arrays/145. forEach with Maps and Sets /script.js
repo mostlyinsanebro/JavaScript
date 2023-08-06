5e52516152381fad8b5f20a660eeae1d8a906439
Abhinav <67469292+mostlyinsanebro@users.noreply.github.com>
2023-08-06 13:33:59 +0530

    Create script.js

---
+++ 11. Working with Arrays/145. forEach with Maps and Sets /script.js
@@ -0,0 +1,23 @@
+// for Each for Maps
+
+const mapdesc = new Map([
+  ["USD", "United States Currency"],
+  ["EUR", "Europen Currency"],
+  ["INR", "Indian Currency"],
+]);
+
+mapdesc.forEach(function (value, key, map) {
+  console.log(`${key} : ${value}`);
+});
+
+// forEACH with sets
+const setdesc = new Set(["USD", "INR", "EUR", "USD", "EUR", "INR"]);
+
+// It prints only unique values.
+console.log(setdesc);
+
+// The signature for callback function is same in the forEach fucntion for a set
+// as it is in the map.
+setdesc.forEach(function (value, key, set) {
+  console.log(`${value}`);
+});
